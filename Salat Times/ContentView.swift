20d5bd35b8ef2679abb55b7aee1126c9c3354d5d
Islam AlorabI <em180423@gmail.com>
2026-01-23 15:08:56 +0200

    Initial Commit

---
+++ Salat Times/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Salat Times
+//
+//  Created by Islam AlorabI on 1/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Salat Times/Salat_TimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salat_TimesApp.swift
+//  Salat Times
+//
+//  Created by Islam AlorabI on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Salat_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
